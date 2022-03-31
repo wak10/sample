f9825e05a0384d69fdc7c7994b2cfdc7a99d6675
wak10 <90087399+wak10@users.noreply.github.com>
2022-03-31 16:42:04 +0900

    Initial Commit

---
+++ Shared/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
