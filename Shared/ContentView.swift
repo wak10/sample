4a60ae70a494ee00b0ec5f694257b04c01655c8f
wak10 <chiba.r25.taka@gmail.com>
2022-04-11 22:09:28 +0900

    [add]

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -18,5 +18,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .environment(\.sizeCategory, .accessibilityMedium)
     }
 }
